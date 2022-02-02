55ac735f536482cb53dc9003d499f9c8e17f1573
Rohan Kulkarni <rohank2502@gmail.com>
2022-02-02 08:12:11 +0530

    headers

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "x-requested-with");
   next();
 });
 
