1a5be231f16f8652ebf172bf74f47c588b0c04cb
Rohan Kulkarni <rohank2502@gmail.com>
2022-02-02 10:51:39 +0530

    remove file

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ app.post('/print', async (req, res) => {
         const response = uploadOnCloudinary(fileName).then(result => {
           try {
             const dir = `${__dirname}/Certificates/${fileName}`
-            fs.rmdir(dir, { recursive: true }, (err) => {
+            fs.unlink(dir, { recursive: true }, (err) => {
               if (err) {
                 throw err;
               }
