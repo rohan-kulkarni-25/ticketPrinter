5b7dd37f92fb9108846c7fd41268739df400d3c8
Rohan Kulkarni <rohank2502@gmail.com>
2022-02-02 08:44:25 +0530

    Added cors

--- app.js
+++ app.js
@@ -8,13 +8,8 @@ const cors = require('cors')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "x-requested-with");
-  next();
-});
 
 
 
